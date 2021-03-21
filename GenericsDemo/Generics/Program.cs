336277df561f1f2e7f1a77c783edf5e94063d7ff
prashikjaware <prashikjaware123@gmail.com>
2021-03-21 09:20:45 +0530

    [Prashik]Added: Print different Data Types using Generics

--- GenericsDemo/Generics/Program.cs
+++ GenericsDemo/Generics/Program.cs
@@ -8,28 +8,9 @@ namespace Generics
 {
     class Program
     {
-
-        public static void  toPrint(int[] inputArray)
+        public static void  toPrint<T>(T[] inputArray)
         {
-            foreach (int element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("---------------------------");
-        }
-
-        public static void toPrint(double[] inputArray)
-        {
-            foreach (double element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("---------------------------");
-        }
-
-        public static void toPrint(char[] inputArray)
-        {
-            foreach (char element in inputArray)
+            foreach (T element in inputArray)
             {
                 Console.WriteLine(element);
             }
